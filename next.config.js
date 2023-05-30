1732a9ed6a6fb3c76b93a8e2f4513b8f79e2df87
AninhaPardini <aninhapardini.work@gmail.com>
2023-05-29 23:31:21 -0300

    🥦 Start pokédex studyng in Nextjs13: Grass and Poison initial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ['raw.githubusercontent.com'],
+    },
+
+}
 
 module.exports = nextConfig
